8f9a61d7d05c09357e7704ee5611f0a48781a94d
Marc Wright <marc@generalassemb.ly>
2019-04-21 21:03:46 +0300

    rendering db messages to the dom, added click events and getting id for each element to send update and delete requests to firebase

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,11 +1,69 @@
-    // Initialize Firebase
-    var config = {
-      apiKey: '<your-api-key>',
-      authDomain: '<your-auth-domain>',
-      databaseURL: '<your-database-url>',
-      storageBucket: '<your-storage-bucket>'
-    };
-    firebase.initializeApp(config);
-    
-$(document).ready(function() {
+firebase.initializeApp(config);
+
+var messageAppReference = firebase.database();
+
+$(() => {
+  console.log(messageAppReference)
+
+  $('#message-form').submit(event => {
+    event.preventDefault()
+ 
+    var message = $('#message').val()
+    $('#message').val('')
+ 
+    var messagesReference = messageAppReference.ref('messages');
+ 
+    messagesReference.push({
+      message: message,
+      votes: 0
+    })
+  })
+
+  function getFanMessages() {
+    messageAppReference
+    .ref('messages')
+    .on('value', (results) => {
+      let allMessages = results.val()
+      let $messageBoardDiv = $('.message-board');
+      
+      for (let msg in allMessages) {        
+        var $upVoteElement = $(`<i class="fa fa-thumbs-up pull-right"></i>`)
+        $upVoteElement.on('click', () => {
+
+        })
+
+        var $downVoteElement = $(`<i class="fa fa-thumbs-down pull-right"></i>`)
+        $downVoteElement.on('click', (e) => {
+          let id = e.target.parentNode.id
+          console.log(id)
+        })        
+        
+        var $deleteElement = $(`<i class="fa fa-trash pull-right delete"></i>`)
+        $deleteElement.on('click', () => {
+
+          console.log("delete")
+        })
+
+        var $votes = $(`<div class="pull-right">${allMessages[msg].votes}</div>`)
+
+        console.log(msg)
+        let $newMessage = $(`<li id=${msg}>${allMessages[msg].message}</li>`);
+
+        $newMessage
+          .append($votes)
+          .append($deleteElement)
+          .append($downVoteElement)
+          .append($upVoteElement)
+
+        $messageBoardDiv.append($newMessage);
+      }
+    })
+  }
+
+
+
+  
+  
+  
+  getFanMessages()
 })
