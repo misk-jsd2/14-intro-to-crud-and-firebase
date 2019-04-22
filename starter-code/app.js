ccdd9523731052315a37a5bc45909c0031ee94c5
Marc Wright <marc@generalassemb.ly>
2019-04-22 10:52:15 +0300

    delete message working

--- starter-code/app.js
+++ starter-code/app.js
@@ -20,8 +20,7 @@ $(() => {
     })
   })  
 
-  function getFanMessages() {
-    
+  function getFanMessages() {    
     messageAppReference
     .ref('messages')
     .on('value', (results) => {
@@ -36,21 +35,38 @@ $(() => {
           console.log(id)
         })
 
+        // UPDATE MESSAGE WITH UPVOTE
+        var $upVoteElement = $(`<i class="fa fa-thumbs-down pull-right"></i>`)
+        $upVoteElement.on('click', (e) => {
+          let id = e.target.parentNode.id
+          console.log(id)
+        }) 
+
+        // UPDATE MESSAGE WITH DOWNVOTE
         var $downVoteElement = $(`<i class="fa fa-thumbs-down pull-right"></i>`)
         $downVoteElement.on('click', (e) => {
           let id = e.target.parentNode.id
           console.log(id)
         })        
         
+        // DELETE MESSAGE
         var $deleteElement = $(`<i class="fa fa-trash pull-right delete"></i>`)
         $deleteElement.on('click', (e) => {
           let id = e.target.parentNode.id
-          console.log(id)
+          
+          messageAppReference
+          .ref(`messages/${id}`)
+          .remove()
+            .then(function() {
+              console.log("Remove succeeded.")
+            })
+            .catch(function(error) {
+              console.log("Remove failed: " + error.message)
+            });
         })
 
         var $votes = $(`<div class="pull-right">${allMessages[msg].votes}</div>`)
 
-        console.log(msg)
         let $newMessage = $(`<li id=${msg}>${allMessages[msg].message}</li>`);
 
         $newMessage
