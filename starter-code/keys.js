649ab570c7ecab3e563851acd81211861d745c4d
Marc Wright <marc.wright@gmail.com>
2019-04-23 21:01:28 +0300

    Delete keys.js

--- starter-code/keys.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAOlnFBv8X42boVHDoBs25xZbC6hH4r6Kc",
-  authDomain: "korn-firebase-app-marc.firebaseapp.com",
-  databaseURL: "https://korn-firebase-app-marc.firebaseio.com",
-  projectId: "korn-firebase-app-marc",
-  storageBucket: "korn-firebase-app-marc.appspot.com",
-  messagingSenderId: "302017691414"
-};
